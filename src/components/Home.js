9dc333e622eb0d8a20857507b994750bc5752b99
jonasmachados <jonasmachado.ti@gmail.com>
2022-12-28 15:10:15 -0300

    Animate Header and Page Base

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,13 +5,8 @@ import { motion } from 'framer-motion';
 const Home = () => {
   return (
     <motion.div className="home container"
-      initial={{ scale: 0 }}
-      animate={{ rotate: 360, scale: 1 }}
-      transition={{
-        type: "spring",
-        stiffness: 260,
-        damping: 20
-      }}
+      initial={{ opacity: 0}}
+      animate={{ opacity: 1 }}
     >
       <h2>
         Welcome to Pizza Joint

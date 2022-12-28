6346b2044efcbb0cc956d08e5ae7ada3a405c739
jonasmachados <jonasmachado.ti@gmail.com>
2022-12-28 14:55:26 -0300

    Animate Page Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
 
 const Home = () => {
   return (
-    <div className="home container">
+    <motion.div className="home container"
+      initial={{ scale: 0 }}
+      animate={{ rotate: 360, scale: 1 }}
+      transition={{
+        type: "spring",
+        stiffness: 260,
+        damping: 20
+      }}
+    >
       <h2>
         Welcome to Pizza Joint
       </h2>
       <Link to="/base">
-        <button>
+        <motion.button
+          animate={{}}
+        >
           Create Your Pizza
-        </button>
+        </motion.button>
       </Link>
-    </div>
+    </motion.div>
   )
 }
 

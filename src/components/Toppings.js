793a3848699cf495ca014f628f6de99c16aa4dd7
jonasmachados <jonasmachado.ti@gmail.com>
2022-12-29 11:19:24 -0300

    Add Variante To Page Orders

--- src/components/Toppings.js
+++ src/components/Toppings.js
@@ -2,11 +2,26 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { motion } from 'framer-motion';
 
+const containerVariants = {
+  hidden: {
+    opacity: 0,
+    x: '100vw'
+  },
+  visible: {
+    opacity: 1,
+    x: 0,
+    transition: { type: 'spring', delay: 0.5 }
+  },
+};
+
 const Toppings = ({ addTopping, pizza }) => {
   let toppings = ['mushrooms', 'peppers', 'onions', 'olives', 'extra cheese', 'tomatoes'];
 
   return (
-    <div className="toppings container">
+    <motion.div className="toppings container"
+      variants={containerVariants}
+      initial="hidden"
+      animate="visible">
 
       <h3>Step 2: Choose Toppings</h3>
       <ul>
@@ -14,8 +29,8 @@ const Toppings = ({ addTopping, pizza }) => {
           let spanClass = pizza.toppings.includes(topping) ? 'active' : '';
           return (
             <motion.li key={topping} onClick={() => addTopping(topping)}
-              whileHover={{scale:1.3, originX: 0, color: '#f8e112'}}
-              transition={{type: 'spring', stiffness: 300}}
+              whileHover={{ scale: 1.3, originX: 0, color: '#f8e112' }}
+              transition={{ type: 'spring', stiffness: 300 }}
             >
               <span className={spanClass}>{topping}</span>
             </motion.li>
@@ -34,7 +49,7 @@ const Toppings = ({ addTopping, pizza }) => {
         </motion.button>
       </Link>
 
-    </div>
+    </motion.div>
   )
 }
 
